d2add73982056d385c93c4ec9d342b820e939147
RU-021 <RU-021@Admins-Mac-mini-4.local>
2013-10-28 12:20:05 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var socketio = require('socket.io')
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//index route
+app.get('/', routes.index);
+ 
+//Create the server
+var server = http.createServer(app)
+
+//Start the web socket server
+var io = socketio.listen(server);
+
+var users = {}
+
+//If the client just connected
+io.sockets.on('connection', function(socket) {
+    users[socket.id] = true
+    socket.emit('message','Server has connected!')
+    io.sockets.emit('userlist',users)
+    socket.on('message', function(data){
+        console.log(data)
+        io.sockets.emit('message',data)
+    }) 
+    socket.on('disconnect',function(data){
+        console.log(data)
+        io.sockets.emit('message','Someone Left...')
+        delete users[socket.id]
+        io.sockets.emit('userlist', users)
+
+    })
+});
+
+
+
+server.listen(3000, function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
---
+++ public/javascripts/main.js
@@ -0,0 +1,30 @@
+$(function(){
+	// connect the socket.io server
+    var socket = io.connect('http://localhost')
+	//define socket events
+    socket.on('connect',function(data){
+        console.log('Connected!')
+        socket.emit('message','A client has connected!')
+    })
+    socket.on('message',function(data){
+        console.log(data)
+        $('#room').append('<p>' + data + '</p>')
+    })
+
+    socket.on('userlist', function(data){
+        $('#users').html('')
+        console.log(data)
+        for (user in data){
+            $('#users').append('<p>' + user + '</p>')
+        }
+    })
+
+	$('#message-input').on('keyup',function(event){
+        console.log(event.which)
+        if (event.which ===13){
+            socket.emit('message', $('#message-input').val())
+            $('#message-input').val('')
+        }
+    })
+	// attach events
+});
